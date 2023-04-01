57d6ea4a349d58e078ce0918df107fcd74fe76c2
mariaqg <mariadelcarmenquinterog@gmail.com>
2023-04-01 11:35:04 +0200

    added file

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import data from "./data";
+import { useState } from "react";
+import List from "./List";
+import Person from "./Person";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  
+  return (
+    <main>
+    <section className='container'>
+    <h3>{people.length} birthdays today</h3>
+    <List people={people} />
+    <button 
+    type='button'
+    className="btn btn-block"
+    onClick={() => setPeople([])}
+    >
+      clear all
+    </button>
+    </section>
+  </main>
+  );
+}
+
+export default App;
